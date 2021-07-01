f7ded547d41cccd90ccd6596cf309bf1011083ea
Jake Appleby <jakeappleby@live.com>
2021-07-01 01:17:51 +0000

    Word search problem.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    let output = [];
+    let lengthOfRows = matrix[0].length;
+    let lengthOfCols = matrix.length;
+    for (let firstCounter = 0; firstCounter < lengthOfRows; firstCounter++) {
+        let rowArray = [];
+        for (let secondCounter = 0; secondCounter < lengthOfCols; secondCounter++) {
+            rowArray.push(matrix[secondCounter][firstCounter]);
+        }
+        output.push(rowArray);
     }
-}
-
-module.exports = wordSearch
+    return output;
+};
+const wordSearch = (letters, word) => {
+    // first check if matrix is empty
+    if (letters.length === 0 || word === '') {
+        return undefined;
+    }
+    const processedWord = word.toUpperCase().trim();
+    // do a search of rows - as is
+    const horizontalJoin = letters.map((ls) => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(processedWord)) {
+            return true;
+        }
+    }
+    // do a search of the col's by first transposing matrix then searchin
+    const verticalJoin = transpose(letters).map((ls) => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(processedWord)) {
+            return true;
+        }
+    }
+    return false;
+};
+module.exports = wordSearch;
